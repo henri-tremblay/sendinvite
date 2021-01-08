46b4664ad0cd9f8a76ed23a13d61d9fe7e79b350
Henri Tremblay <henri.tremblay@gmail.com>
2021-01-07 22:40:16 -0500

    Create an invite

--- src/main/java/CalendarFactory.java
+++ src/main/java/CalendarFactory.java
@@ -7,12 +7,9 @@ import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.util.DateTime;
 import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.calendar.Calendar;
 import com.google.api.services.calendar.CalendarScopes;
-import com.google.api.services.calendar.model.Event;
-import com.google.api.services.calendar.model.Events;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -20,20 +17,25 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.security.GeneralSecurityException;
-import java.util.Collections;
 import java.util.List;
 
-public class CalendarQuickstart {
-    private static final String APPLICATION_NAME = "Google Calendar API Java Quickstart";
+public class CalendarFactory {
+    private static final String APPLICATION_NAME = "jChampions Conference Send Invitation";
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
     private static final String TOKENS_DIRECTORY_PATH = "tokens";
 
     /**
-     * Global instance of the scopes required by this quickstart.
      * If modifying these scopes, delete your previously saved tokens/ folder.
      */
-    private static final List<String> SCOPES = Collections.singletonList(CalendarScopes.CALENDAR_READONLY);
+    private static final List<String> SCOPES = List.of(CalendarScopes.CALENDAR);
     private static final String CREDENTIALS_FILE_PATH = "credentials.json";
+
+    public Calendar newCalendar() throws GeneralSecurityException, IOException {
+        NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        return new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+    }
 
     /**
      * Creates an authorized Credential object.
@@ -41,7 +43,7 @@ public class CalendarQuickstart {
      * @return An authorized Credential object.
      * @throws IOException If the credentials.json file cannot be found.
      */
-    private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
+    private Credential getCredentials(NetHttpTransport HTTP_TRANSPORT) throws IOException {
         // Load client secrets.
         BufferedReader in = Files.newBufferedReader(Path.of(CREDENTIALS_FILE_PATH), StandardCharsets.UTF_8);
         GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, in);
@@ -54,35 +56,5 @@ public class CalendarQuickstart {
                 .build();
         LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
         return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
-    }
-
-    public static void main(String... args) throws IOException, GeneralSecurityException {
-        // Build a new authorized API client service.
-        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-        Calendar service = new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
-                .setApplicationName(APPLICATION_NAME)
-                .build();
-
-        // List the next 10 events from the primary calendar.
-        DateTime now = new DateTime(System.currentTimeMillis());
-        Events events = service.events().list("primary")
-                .setMaxResults(10)
-                .setTimeMin(now)
-                .setOrderBy("startTime")
-                .setSingleEvents(true)
-                .execute();
-        List<Event> items = events.getItems();
-        if (items.isEmpty()) {
-            System.out.println("No upcoming events found.");
-        } else {
-            System.out.println("Upcoming events");
-            for (Event event : items) {
-                DateTime start = event.getStart().getDateTime();
-                if (start == null) {
-                    start = event.getStart().getDate();
-                }
-                System.out.printf("%s (%s)\n", event.getSummary(), start);
-            }
-        }
     }
 }
--- src/main/java/SendInvite.java
+++ src/main/java/SendInvite.java
@@ -1,90 +1,82 @@
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.DateTime;
-import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.calendar.Calendar;
-import com.google.api.services.calendar.CalendarScopes;
 import com.google.api.services.calendar.model.Event;
 import com.google.api.services.calendar.model.EventAttendee;
 import com.google.api.services.calendar.model.EventDateTime;
 
-import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Collections;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class SendInvite {
-    private static final String APPLICATION_NAME = "jChampions Conference Send Invitation";
-    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
-    private static final String TOKENS_DIRECTORY_PATH = "tokens";
 
-    /**
-     * If modifying these scopes, delete your previously saved tokens/ folder.
-     */
-    private static final List<String> SCOPES = List.of(CalendarScopes.CALENDAR);
-    private static final String CREDENTIALS_FILE_PATH = "credentials.json";
+    private final Calendar calendar;
 
-    /**
-     * Creates an authorized Credential object.
-     * @param HTTP_TRANSPORT The network HTTP Transport.
-     * @return An authorized Credential object.
-     * @throws IOException If the credentials.json file cannot be found.
-     */
-    private static Credential getCredentials(NetHttpTransport HTTP_TRANSPORT) throws IOException {
-        // Load client secrets.
-        BufferedReader in = Files.newBufferedReader(Path.of(CREDENTIALS_FILE_PATH), StandardCharsets.UTF_8);
-        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, in);
-
-        // Build flow and trigger user authorization request.
-        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
-                .setAccessType("offline")
-                .build();
-        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
-        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+    public SendInvite(Calendar calendar) {
+        this.calendar = calendar;
     }
 
     public static void main(String[] args) throws Exception {
-        NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-        Calendar service = new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
-                .setApplicationName(APPLICATION_NAME)
-                .build();
+        CalendarFactory calendarFactory = new CalendarFactory();
+        Calendar calendar = calendarFactory.newCalendar();
+        SendInvite service = new SendInvite(calendar);
 
+        Files.lines(Paths.get("slots.csv"), StandardCharsets.UTF_8)
+                .skip(1)
+                .map(line -> line.split(";"))
+                .map(tokens -> new Object() {
+                    final String summary = tokens[0];
+                    final String location = tokens[1];
+                    final String description = tokens[2];
+                    final String startTime = tokens[3];
+                    final String endTime = tokens[4];
+                    final String speaker = tokens[5];
+
+                })
+                .forEach(tuple -> service.createInvitation(
+                        tuple.summary,
+                        tuple.location,
+                        tuple.description,
+                        tuple.startTime,
+                        tuple.endTime,
+                        tuple.speaker
+                ));
+    }
+
+    private void createInvitation(String summary, String location, String description, String startTime, String endTime, String speaker) {
         Event event = new Event()
-                .setSummary("jChampionConf Test")
-                .setLocation("http://streamyard.com")
-                .setDescription("Henri will be presenting some cool stuff");
+                .setSummary(summary)
+                .setLocation(location)
+                .setDescription(description);
 
-        DateTime startDateTime = new DateTime("2021-01-07T23:00:00-05:00");
+        DateTime startDateTime = new DateTime(startTime);
         EventDateTime start = new EventDateTime()
                 .setDateTime(startDateTime)
                 .setTimeZone("America/Toronto");
         event.setStart(start);
 
-        DateTime endDateTime = new DateTime("2021-01-07T23:30:00-05:00");
+        DateTime endDateTime = new DateTime(endTime);
         EventDateTime end = new EventDateTime()
                 .setDateTime(endDateTime)
                 .setTimeZone("America/Toronto");
         event.setEnd(end);
 
         List<EventAttendee> attendees = List.of(
-                new EventAttendee().setEmail("jessica.tornare@gmail.com")
+                new EventAttendee().setEmail(speaker)
         );
         event.setAttendees(attendees);
 
         String calendarId = "primary";
-        event = service.events().insert(calendarId, event).execute();
+        try {
+            event = calendar.events().insert(calendarId, event)
+                    .setSendNotifications(true)
+                    .execute();
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
         System.out.printf("Event created: %s\n", event.getHtmlLink());
     }
 }
